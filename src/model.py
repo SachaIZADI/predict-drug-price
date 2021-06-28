54242487960e3343fafeb284f57b00955abaabe8
Sacha Izadi <izadi.sacha@bcg.com>
2021-06-28 23:51:03 +0200

    add gridsearch parameters tuning in pipeline

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
 
-model = XGBRegressor()
+Model = XGBRegressor
+model = Model()
--- src/training_pipeline.py
+++ src/training_pipeline.py
@@ -1,10 +1,13 @@
 from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GridSearchCV
 
 from src.feature_engineering.feature_builder import FeatureBuilder
 from src.model import model
 
 
-def train_model():
+def train_model(
+    use_grid_search: bool = True
+):
 
     FEATURES = [
         'label_plaquette', 'label_ampoule',
@@ -41,5 +44,23 @@ def train_model():
         scores['test_score'].mean()
     )
 
+    if use_grid_search:
+
+        param_grid = {
+            "n_estimators": [30, 50, 100, 200],
+            "max_depth": [3, 5, 10, 15],
+            "learning_rate": [0.05, 0.10, 0.15, 0.20, 0.25, 0.30]
+        }
+        grid_search = GridSearchCV(model, param_grid, scoring='r2', refit=True, cv=5)
+        grid_search.fit(X_train, y_train)
+
+        print(grid_search.best_score_)
+        print(grid_search.best_params_)
+
 
     # model.fit(X_train, y_train)
+
+    # /Users/izadisacha/Documents/MM_case/venv/bin/python /Users/izadisacha/Documents/MM_case/src/__main__.py
+    # 0.6402693071000864 0.3240530179893231
+    # 0.3636898504750632
+    # {'learning_rate': 0.15, 'max_depth': 5, 'n_estimators': 100}
