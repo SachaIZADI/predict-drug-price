55e668523db7003b4457d9c66db4f4bd3302470d
Sacha Izadi <izadi.sacha@bcg.com>
2021-06-28 20:15:06 +0200

    create data loader method

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import git
+from pathlib import Path
+
+
+def get_git_root() -> Path:
+    git_repo = git.Repo("", search_parent_directories=True)
+    git_root = git_repo.git.rev_parse("--show-toplevel")
+    return Path(git_root)
